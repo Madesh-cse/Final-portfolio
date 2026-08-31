c6fb61f8ccb14fd535708cc2fd2cb9fd4769f0ff
Madesh <madeshdex10@gmail.com>
2026-08-31 20:05:55 +0530

    install the depedency of analytics view

--- my-portfolio/src/main.tsx
+++ my-portfolio/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import "./styles/Pages/_root.scss"
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+     <Analytics />
   </StrictMode>,
 )
